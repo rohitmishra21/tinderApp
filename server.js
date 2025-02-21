cbeddce82a3ec94c963220d3da347e97072d2f58
Rohit mishra <mishrarohit27112005@gmail.com>
2025-02-21 00:18:45 -0800

    complete backend

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ const connect = require("./src/db/db");
 connect()
   .then(() => {
     console.log("db connected");
-    app.listen(3000,()=>{
+    app.listen(3000, () => {
       console.log("server is running on port 3000");
-      
-    })
+    });
   })
   .catch((err) => {
     console.error("db is not connected");
--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { userAuth } = require("../middleware/auth");
 const RequestConnectionModel = require("../models/connectionRequest");
 const userRoute = express.Router();
+const User = require("../models/user");
 
 const POPULATE_DATA = "firstName lastName profileImg gender age skills bio";
 
@@ -45,13 +46,33 @@ userRoute.get("/user/request/connection", userAuth, async (req, res) => {
 userRoute.get("/user/feed", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
 
     const connectionRequest = await RequestConnectionModel.find({
       $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
     }).select("fromUserId toUserId");
 
-    const hideUserFromFeed = new set();
-    
+    const hideUserFromFeed = new Set();
+
+    connectionRequest.forEach((req) => {
+      hideUserFromFeed.add(req.fromUserId.toString()),
+        hideUserFromFeed.add(req.toUserId.toString());
+    });
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUserFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select(POPULATE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    res.send(users);
   } catch (error) {
     res.status(400).send(error.message);
   }
